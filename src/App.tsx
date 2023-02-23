18b4dc931cfa4d5161c1b3daacf3be4b5463f49a
Lucas Santos <LuSantosatl@gmail.com>
2023-02-23 16:49:30 -0500

    Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import { useState } from "react";
+// Components
+import Home from "./pages/home";
+import Store from "./pages/store";
+import About from "./pages/about";
+import Navbar from "./components/Navbar";
+
+// React Imports
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App">hello</div>;
+  return (
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/store" element={<Store />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </Container>
+    </>
+  );
 }
 
 export default App;
